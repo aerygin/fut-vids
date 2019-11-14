acad5ed3abe2c5ed87a506890a8cda22f4197349
aerygin <artjom.jerogin@gmail.com>
2019-11-14 22:55:50 +0200

    git init

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="home">
+        <h2>FutVids</h2>
+        <v-btn rounded>Hello</v-btn>
+    </div>
+</template>
+
+<script>
+    // @ is an alias to /src
+    import axios from 'axios';
+
+    export default {
+        name: 'home',
+        components: {
+        },
+        data: () => ({
+            res:[]
+        }),
+        mounted() {
+            axios.get("https://www.scorebat.com/video-api/v1/")
+                .then(response => {
+                    this.res = response.data;
+                })
+        }
+
+    }
+</script>
