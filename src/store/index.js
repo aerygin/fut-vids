dc420702a2d20b6268a7ca92ce3b7b7d36ce81d7
aerygin <artjom.jerogin@gmail.com>
2019-11-14 23:57:40 +0200

    vuex store configured

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,42 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        matches: [],
+        isLoading: true,
+        error: {
+            isError: false,
+            message: ""
+        },
+    },
+    mutations: {
+        addMatches(state, data) {
+            state.matches = data;
+        },
+        finishLoading(state) {
+            state.isLoading = false;
+        },
+        setErrorTrue(state, message) {
+            state.error.isError = true;
+            state.error.message = message;
+        }
+    },
+    actions: {
+        getAllMatches(store) {
+            axios.get("https://www.scorebat.com/video-api/v1/")
+                .then(response => {
+                    store.commit('addMatches', response.data);
+                    store.commit('finishLoading');
+                })
+                .catch(error => {
+                    store.commit('setErrorTrue', error);
+                })
+        }
+    },
+    modules: {
+    }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,55 @@
 <template>
-    <div class="home">
-        <h2>FutVids</h2>
-        <v-btn rounded>Hello</v-btn>
+    <div class="main-wrapper">
+        <div class="main-wrapper">
+            <v-progress-circular v-if="isLoading"
+                                 size=100
+                                 color="#1565C0"
+                                 width="7"
+                                 :indeterminate="indeterminate">
+            </v-progress-circular>
+            <div v-else class="matches">
+                <h3>Latest videos</h3>
+                <div v-for="(match,index) in matches"
+                     :key="index"
+                     class="match-item">
+                    {{match.title}}
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     // @ is an alias to /src
-    import axios from 'axios';
 
     export default {
         name: 'home',
         components: {
         },
         data: () => ({
-            res:[]
+            indeterminate: true
         }),
-        mounted() {
-            axios.get("https://www.scorebat.com/video-api/v1/")
-                .then(response => {
-                    this.res = response.data;
-                })
+        computed: {
+            isLoading() {
+                return this.$store.state.isLoading;
+            },
+            matches() {
+                return this.$store.state.matches;
+            }
+        },
+        methods: {
+
+        },
+        created() {
+            this.$store.dispatch('getAllMatches');
         }
+
 
     }
 </script>
+<style scoped>
+    .main-wrapper {
+        display: flex;
+        justify-content: center;
+    }
+</style>
